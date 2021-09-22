50ad6492812d89864a95e6f092b388934c97d2b9
fatmah <famosaal2011@gmail.com>
2021-09-22 22:04:09 +0300

    Rewrite Question

--- bikeshare.py
+++ bikeshare.py
@@ -1,4 +1,4 @@
-import time 
+import time
 import pandas as pd
 import numpy as np
 
@@ -7,18 +7,18 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'washington': 'washington.csv' }
 
 def get_filters():
-      
+
     print('Hello! Let\'s explore some US bikeshare data!')
-    city = input('choose city name?').lower()
+    city = input('which city would you like to see data chicago,washington or new_york_city?').lower()
     while city not in (CITY_DATA .keys()):
-        print('incorrect city name')
+        print('which city would you like to see data chicago,washington or new_york_city?')
         city= input('choose city name?').lower()
-  
+
     filter= input( 'Do you want to filter data by month,day,or all?').lower()
     while filter not in (['month','day','all']):
         print('incorrect filter')
         filter= input('Do you want to filter data by month,day,or all?').lower()
-     
+
     months =['january','february','march','april','may','june']
     if filter == 'month' or filter == 'all' :
         month = input('which month: January,February,March,April,May,June?').lower()
@@ -27,7 +27,7 @@ def get_filters():
             month = input('which month: January,February,March,April,May,June?').lower()
     else:
         month = 'all'
- 
+
     days =['monday','tuesday','wednsday','thursday','friday','saturday','sunday']
     if filter == 'day' or filter == 'all' :
        day = input('which day:Monday,Tuesday,Wednsday,Thursday,Friday,Sunday ?').lower()
@@ -36,7 +36,7 @@ def get_filters():
             day = input('which day:Monday,Tuesday,Wednsday,Thursday,Friday,Sunday ?').lower()
     else:
          day  = 'all'
-        
+
     print('-'*40)
     return city, month, day
 
@@ -61,9 +61,9 @@ def load_data(city, month, day):
         df = df[df['day_of_week'] == day.title()]
 
     return df
-          
-    
-def time_stats(df): 
+
+
+def time_stats(df):
     print('\nCalculatin The Most Frequent Times of Travel ...\n')
     start_time = time.time()
     print(df.month.mode()[0])
@@ -80,18 +80,18 @@ def station_stats(df):
     print('\nCalculating The Most Popular Station and Trip...\n')
     start_time = time.time()
     common_start = df['Start Station'].mode()[0]
-    print(common_start)      
-    common_end = df['End Station'].mode()[0]      
+    print(common_start)
+    common_end = df['End Station'].mode()[0]
     print(common_end)
-    df['combination'] = df['Start Station'] + ' to ' + df['End Station']      
+    df['combination'] = df['Start Station'] + ' to ' + df['End Station']
     common_combination = df['combination'].mode()[0]
-    print( common_combination) 
-          
+    print( common_combination)
+
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40) 
-          
-def trip_duration_stats(df): 
-       
+    print('-'*40)
+
+def trip_duration_stats(df):
+
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
     total_travel = df['Trip Duration'].sum()
@@ -103,18 +103,18 @@ def trip_duration_stats(df):
 
 
 def user_stats(df,city):
-   
+
     print('\nCalculating User Stats...\n')
     start_time = time.time()
     user_types = df['User Type'].value_counts()
-    print(user_types) 
+    print(user_types)
     print(df.columns)
     if 'Gender'in df.columns.values.tolist():
         gender = df['Gender'].value_counts()
         print(gender)
-    else: 
+    else:
         print("Not applicable on this city.")
-        
+
     if 'Birth Year'in df.columns.values.tolist():
         earliest = df['Birth Year'].min()
         print(earliest)
@@ -124,33 +124,33 @@ def user_stats(df,city):
         print(common_birth)
     else:
         print("Not applicable on this city.")
-        
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-    
-     
-           
+
+
+
 def main():
    while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-        
+
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
-     
+
         n = 0
         while True :
             entry= input("Do you want to print raw data?").lower()
-            if entry  == "yes" :     
+            if entry  == "yes" :
                 print (df.iloc[n:n+5])
                 n += 5
             elif entry == "no":
                 break
-                
+
         print(df.head())
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
